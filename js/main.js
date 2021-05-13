a00b005900df0c75621891773ba2994aa83e4ddc
이지훈 <easyhoon119@github.com>
2021-05-14 05:12:10 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    // 1초마다 시간 갱신하기
+    setInterval(() => {
+        var now = document.querySelector('.clock');
+        var date = Date();
+        now.innerHTML = date;
+    }, 1000);
+    // 타이틀 옆에 사진 띄우기
+    var title = document.querySelector('.title');
+    var face = document.querySelector('.my_face');
+    title.addEventListener('click', function () {
+        face.style.display = 'block';
+    });
+    // button 동작
+    var button1 = document.querySelector('#button1');
+    var button2 = document.querySelector('#button2');
+    var button3 = document.querySelector('#button3');
+    var home = document.querySelector('.fa-home');
+
+    home.addEventListener('click', function () {
+        window.location.href = './index.html';
+    });
+
+    button1.addEventListener('click', function () {
+        window.location.href = './my_project1.html';
+    });
+};
---
+++ js/project.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+    // 1초마다 시간 갱신하기
+    setInterval(() => {
+        var now = document.querySelector('.clock');
+        var date = Date();
+        now.innerHTML = date;
+    }, 1000);
+    //버튼 동작
+    var button4 = document.querySelector('#button4');
+    var button5 = document.querySelector('#button5');
+    var button6 = document.querySelector('#button6');
+    var home = document.querySelector('.fa-home');
+
+    home.addEventListener('click', function () {
+        window.location.href = './index.html';
+    });
+
+    //타이틀 클릭시 reload
+    var title = document.querySelector('.title');
+
+    title.addEventListener('click', function () {
+        window.location.reload();
+    });
+
+    //iframe 동작
+    var blossom = document.querySelector('.blossom');
+    var predict_face = document.querySelector('.predict_face');
+    var pygame = document.querySelector('.pygame');
+    blossom.style.display = 'none';
+    predict_face.style.display = 'none';
+    pygame.style.display = 'none';
+
+    button4.addEventListener('click', function () {
+        predict_face.style.display = 'none';
+        blossom.style.display = 'block';
+        pygame.style.display = 'none';
+        button4.style.backgroundColor = 'gray';
+        button5.style.backgroundColor = 'greenyellow';
+        button6.style.backgroundColor = 'greenyellow';
+    });
+
+    button5.addEventListener('click', function () {
+        predict_face.style.display = 'block';
+        blossom.style.display = 'none';
+        pygame.style.display = 'none';
+        button5.style.backgroundColor = 'gray';
+        button4.style.backgroundColor = 'greenyellow';
+        button6.style.backgroundColor = 'greenyellow';
+    });
+
+    button6.addEventListener('click', function () {
+        pygame.style.display = 'block';
+        predict_face.style.display = 'none';
+        blossom.style.display = 'none';
+        button6.style.backgroundColor = 'gray';
+        button5.style.backgroundColor = 'greenyellow';
+        button4.style.backgroundColor = 'greenyellow';
+    });
+};
