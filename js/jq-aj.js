0e3bb73515800bf9819c8d476e6e30613a2c317f
이지훈 <easyhoon119@github.com>
2021-05-28 02:12:51 +0900

    first complete project

--- js/jq-aj.js
+++ js/jq-aj.js
@@ -14,6 +14,7 @@ window.onload = function () {
     var toggle_exam = document.querySelector('.toggle-container');
     var spread = document.querySelector('.spreadSheet-container');
     var explain = document.querySelector('.explain');
+    var weather_api = document.querySelector('.weather-container');
     var home = document.querySelector('.fa-home');
 
     home.addEventListener('click', function () {
@@ -25,10 +26,12 @@ window.onload = function () {
         toggle_exam.style.display = 'none';
         spread.style.display = 'none';
         explain.style.display = 'none';
+        weather_api.style.display = 'none';
 
         button1.style.backgroundColor = 'grey';
         button4.style.backgroundColor = 'greenyellow';
         button2.style.backgroundColor = 'greenyellow';
+        button3.style.backgroundColor = 'greenyellow';
 
         //슬라이더 구현
         $('.slider').bxSlider();
@@ -39,10 +42,12 @@ window.onload = function () {
         toggle_exam.style.display = 'flex';
         spread.style.display = 'none';
         explain.style.display = 'none';
+        weather_api.style.display = 'none';
 
         button1.style.backgroundColor = 'greenyellow';
         button4.style.backgroundColor = 'grey';
         button2.style.backgroundColor = 'greenyellow';
+        button3.style.backgroundColor = 'greenyellow';
     });
 
     button2.addEventListener('click', function () {
@@ -50,10 +55,25 @@ window.onload = function () {
         toggle_exam.style.display = 'none';
         spread.style.display = 'block';
         explain.style.display = 'none';
+        weather_api.style.display = 'none';
 
         button1.style.backgroundColor = 'greenyellow';
         button4.style.backgroundColor = 'greenyellow';
         button2.style.backgroundColor = 'grey';
+        button3.style.backgroundColor = 'greenyellow';
+    });
+
+    button3.addEventListener('click', function () {
+        slider.style.display = 'none';
+        toggle_exam.style.display = 'none';
+        spread.style.display = 'none';
+        explain.style.display = 'none';
+        weather_api.style.display = 'flex';
+
+        button1.style.backgroundColor = 'greenyellow';
+        button4.style.backgroundColor = 'greenyellow';
+        button2.style.backgroundColor = 'greenyellow';
+        button3.style.backgroundColor = 'grey';
     });
     //타이틀 클릭시 reload
     var title = document.querySelector('.title');
@@ -92,5 +112,56 @@ window.onload = function () {
             }
         });
     });
-
+    //날씨 api 구현하기
+    var weather1 = document.querySelector('.weather1');
+    var weather2 = document.querySelector('.weather2');
+    var weather3 = document.querySelector('.weather3');
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=8f152b03cee54f5c6f59fd728db26f32",
+        dataType: "json",
+        type: "GET",
+        async: "false",
+        success: function (resp) {
+            weather1.innerHTML = "나라 : " + resp.sys.country + "<br/>";
+            weather1.innerHTML += "도시 : " + resp.name + "<br/>";
+            weather1.innerHTML += "현재 온도 : " + (resp.main.temp - 273.15) + "'c" + "<br/>";
+            weather1.innerHTML += "현재 습도 : " + resp.main.humidity + "%" + "<br/>";
+            weather1.innerHTML += "현재 날씨 : " + resp.weather[0].main + "<br/>";
+            weather1.innerHTML += "상세 날씨 : " + resp.weather[0].description + "<br/>";
+            weather1.innerHTML += "바람 세기 : " + resp.wind.speed + "<br/>";
+            weather1.innerHTML += "구름양 : " + resp.clouds.all + "%" + "<br/>";
+        }
+    });
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=8f152b03cee54f5c6f59fd728db26f32",
+        dataType: "json",
+        type: "GET",
+        async: "false",
+        success: function (resp) {
+            weather2.innerHTML = "나라 : " + resp.sys.country + "<br/>";
+            weather2.innerHTML += "도시 : " + resp.name + "<br/>";
+            weather2.innerHTML += "현재 온도 : " + (resp.main.temp - 273.15) + "'c" + "<br/>";
+            weather2.innerHTML += "현재 습도 : " + resp.main.humidity + "%" + "<br/>";
+            weather2.innerHTML += "현재 날씨 : " + resp.weather[0].main + "<br/>";
+            weather2.innerHTML += "상세 날씨 : " + resp.weather[0].description + "<br/>";
+            weather2.innerHTML += "바람 세기 : " + resp.wind.speed + "<br/>";
+            weather2.innerHTML += "구름양 : " + resp.clouds.all + "%" + "<br/>";
+        }
+    });
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=Washington&appid=8f152b03cee54f5c6f59fd728db26f32",
+        dataType: "json",
+        type: "GET",
+        async: "false",
+        success: function (resp) {
+            weather3.innerHTML = "나라 : " + resp.sys.country + "<br/>";
+            weather3.innerHTML += "도시 : " + resp.name + "<br/>";
+            weather3.innerHTML += "현재 온도 : " + (resp.main.temp - 273.15) + "'c" + "<br/>";
+            weather3.innerHTML += "현재 습도 : " + resp.main.humidity + "%" + "<br/>";
+            weather3.innerHTML += "현재 날씨 : " + resp.weather[0].main + "<br/>";
+            weather3.innerHTML += "상세 날씨 : " + resp.weather[0].description + "<br/>";
+            weather3.innerHTML += "바람 세기 : " + resp.wind.speed + "<br/>";
+            weather3.innerHTML += "구름양 : " + resp.clouds.all + "%" + "<br/>";
+        }
+    });
 };
